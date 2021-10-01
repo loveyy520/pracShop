fd8f55442a1d7803c5bc4ed841a1523491d53e23
Loveyy <201357337@qq.com>
2021-10-01 12:23:12 +0800

    接口改动

--- src/network/request.js
+++ src/network/request.js
@@ -3,15 +3,15 @@ import axios from 'axios'
 export function request(config) {
     // 创建实例，否则为全局
     const ins = axios.create({
-        baseURL: 'http://152.136.185.210:7878/api/m5',
-        timeout:5000
+        baseURL: 'http://152.136.185.210:7878/api/hy66',
+        timeout: 5000
     })
 
     // 拦截请求
     ins.interceptors.request.use(config => {
         return config
     }, err => {
-        
+
     })
 
     // 拦截响应
